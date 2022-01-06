61f715360bc6f49a2731869e2f2633ed4d6855ff
Oscar Silva <oscarsilva2356@gmail.com>
2022-01-06 11:19:54 -0800

    js w

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
+
+// use the div to identify which hour where at. 
+// use moment for the current hour. 
+// use IF statements to say which is the current hour, and for which color to go for. 
