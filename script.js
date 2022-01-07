9735adb8962c669f1939a7d5e9a41f937bc85111
Oscar Silva <oscarsilva2356@gmail.com>
2022-01-07 14:57:46 -0800

    js d

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var hour9 = document.getElementById("#hour-9"); 
-var hour10 = document.getElementById("#hour-10"); 
-var hour11 = document.getElementById("#hour-11"); 
+var hour9 = $("#hour-9"); 
+var hour10 = d("#hour-10"); 
+var hour11 = d("#hour-11"); 
 var hour12 = document.getElementById("#hour-12"); 
 var hour13 = document.getElementById("#hour-13"); 
 var hour14 = document.getElementById("#hour-14"); 
@@ -15,8 +15,20 @@ var description = document.querySelector(".description")
 
 var saveBtnEl = $(".saveBtn");
 
-var time = moment().format("MMM Do, YYYY", "hh:mm:ss");
+var time = moment().format("MMM Do, YYYY");
 $("#currentDay").text(time);
+
+
+
+localStorage.setItem('hour-9', '');
+
+
+
+
+//Add an event to run function when they click save button in the hour 9 div
+// Use DOM teaverseal to get to textarea, Use varable to assign
+// Get the vlue of the texxt area and ssign to varable 
+// write that vlue with the key hour-9 in localStorage 
 
 function printTextHour() {
     var task = localStorage.getItem("task")
@@ -24,6 +36,7 @@ function printTextHour() {
         return;
     }
 }
+
 
 
 
