e5093cdb8e0b455f2ce616cb3c7797c231808196
Oscar Silva <oscarsilva2356@gmail.com>
2022-01-07 22:58:31 -0800

    push

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # bc-HomeWork5
+This is a schedule that displays the current hour for the past, present and future by color. Users can aslo write task on the lines. 
+
+This is not completed code. This was uploaded for the deadline. 
--- script.js
+++ script.js
@@ -1,41 +1,25 @@
 var hour9 = $("#hour-9"); 
-var hour10 = d("#hour-10"); 
-var hour11 = d("#hour-11"); 
-var hour12 = document.getElementById("#hour-12"); 
-var hour13 = document.getElementById("#hour-13"); 
-var hour14 = document.getElementById("#hour-14"); 
-var hour15 = document.getElementById("#hour-15"); 
-var hour16 = document.getElementById("#hour-16"); 
-var hour17 = document.getElementById("#hour-17"); 
-var past = document.querySelector(".past"); 
-var present = document.querySelector(".present"); 
-var furture = document.querySelector(".future"); 
-var description = document.querySelector(".description")
-
-
+var hour10 = $("#hour-10"); 
+var hour11 = $("#hour-11"); 
+var hour12 = $("#hour-12"); 
+var hour13 = $("#hour-13"); 
+var hour14 = $("#hour-14"); 
+var hour15 = $("#hour-15"); 
+var hour16 = $("#hour-16"); 
+var hour17 = $("#hour-17"); 
 var saveBtnEl = $(".saveBtn");
-
 var time = moment().format("MMM Do, YYYY");
 $("#currentDay").text(time);
 
 
 
-localStorage.setItem('hour-9', '');
+saveBtnEl.on("click", function() {
+    var time = $(this).parent().attr("id");
+    var text = $(this).siblings(".description").val();
+    localStorage.setItem(time, text);
+    console.log(time, text);
 
-
-
-
-//Add an event to run function when they click save button in the hour 9 div
-// Use DOM teaverseal to get to textarea, Use varable to assign
-// Get the vlue of the texxt area and ssign to varable 
-// write that vlue with the key hour-9 in localStorage 
-
-function printTextHour() {
-    var task = localStorage.getItem("task")
-    if (task === null) {
-        return;
-    }
-}
+}); 
 
 
 
@@ -46,17 +30,12 @@ function printTextHour() {
 
 
 
- 
 
 
 
 
 
 
-
-
-// use moment for the current hour. 
-// use IF statements to say which is the current hour, and for which color to go for. 
 
 
 
